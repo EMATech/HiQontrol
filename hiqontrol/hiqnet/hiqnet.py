bfa5da04cf3d6f31cd720de16b9a4f4fd639e853
Raphaël Doursenaud <rdoursenaud@free.fr>
2015-05-14 18:22:49 +0200

    Moved flags to a separate file and implemented parameter flags

--- hiqontrol/hiqnet/hiqnet.py
+++ hiqontrol/hiqnet/hiqnet.py
@@ -10,11 +10,9 @@ import netifaces
 import socket
 import random
 import binascii
-import ctypes
+from flags import *
 
 from twisted.internet import protocol
-
-c_uint16 = ctypes.c_uint16
 
 IP_PORT = 3804  # IANA declared as IQnet. Go figure.
 
@@ -80,49 +78,6 @@ MSG_REQEVTLOG            = b'\x01\x2c'
 # Virtual devices, objects and parameters
 #   Have a Class Name and a Class ID
 
-class DeviceFlagsBits(ctypes.LittleEndianStructure):
-    """Bitfields for the device flags."""
-    _fields_ = [
-        ('reqack', c_uint16, 1),
-        ('ack', c_uint16, 1),
-        ('info', c_uint16, 1),
-        ('error', c_uint16, 1),
-        ('res1', c_uint16, 1),
-        ('guaranteed', c_uint16, 1),
-        ('multipart', c_uint16, 1),
-        ('res2', c_uint16, 1),
-        ('session', c_uint16, 1),
-        ('res3', c_uint16, 1),
-        ('res4', c_uint16, 1),
-        ('res5', c_uint16, 1),
-        ('res6', c_uint16, 1),
-        ('res7', c_uint16, 1),
-        ('res8', c_uint16, 1),
-        ('res9', c_uint16, 1),
-    ]
-
-
-class DeviceFlags(ctypes.Union):
-    """Device flags."""
-    _fields_ = [
-        ('b', DeviceFlagsBits),
-        ('asByte', c_uint16),
-    ]
-
-    _anonymous_ = 'b'
-
-    def __bytes__(self):
-        return struct.pack('!H', self.asByte)
-
-    def __str__(self):
-        return self.__bytes__()
-
-    def __add__(self, other):
-        return str(self) + other
-
-    def __radd__(self, other):
-        return other + str(self)
-
 
 class Attribute:
     """Member variables of the HiQnet architecture.
@@ -170,12 +125,7 @@ class Parameter:
     minimum_value = None  # Depends on data_type
     maximum_value = None  # Depends on data_type
     control_law = None
-    flags = 0
-    """
-    Bits 0, 2, and 3 are reserved. Bit 1 is the Sensor Attribute.
-        0 = Non-Sensor
-        1 = Sensor
-    """
+    flags = ParameterFlags()
 
 
 class NetworkInfo:
